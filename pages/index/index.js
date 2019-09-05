26d76b55181a5ba416b883160ab0194dde814558
wcy <13813375894@163.com>
2019-09-05 22:31:28 +0800

    0905

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,32 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'coderwhy',
+    class: 'hloo',
+    students: [
+      { id: 110, name: "zhangsan", age: 30 },
+      { id: 110, name: "zhangsan", age: 30 },
+      { id: 120, name: "lisi", age: 31 },
+      { id: 130, name: "wangwu", age: 32 },
+      { id: 132, name: "wangwu1", age: 32 }
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    console.log("按钮发生点击")
+    //this.data.counter += 1
+    console.log(this.data.counter)
+    this.setData(
+      {
+        counter: this.data.counter + 1
+      }
+    )
   },
+  handleBtnSubtraction() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData(
+      {
+        counter: this.data.counter - 1
+      }
+    )
   }
 })
